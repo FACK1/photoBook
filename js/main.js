ac97533626d4e606b0d877d8c5b7d3fc77cb5520
abozanona <abozanona@gmail.com>
2018-11-07 13:47:21 +0200

    Fix apis

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+function sendMessage(img){
+  fetch(`https://graph.facebook.com/v2.6/me/messages?access_token=${fb_page_access_token}`, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        recipient : {
+          id: "1855999237796614"
+        },
+        message:{
+            attachment:{
+            type:"image", 
+            payload:{
+              url:"https://picsum.photos/400/400/?image=358", 
+              is_reusable:true
+            }
+          }
+        }
+      })
+  }).then(res=>res.json())
+    .then(res => alert("Message Sent Succ."));
+}
 
 function getRangomImage(fn, height=400, width=400){
   var apiRandomPhoto = `https://picsum.photos/${height}/${width}/?random`;
@@ -16,10 +40,43 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+
+function getGifImage(fn, q){
+  var apiGif = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}`;
+  fetch(apiGif)
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(res){
+    fn(res.data[getRandomInt(res.data.length-1)].images.downsized_large.url);
+  });
+}
+
+function setGifImage(el, q){
+  getGifImage(function(url){
+      el.src = url;
+  }, q);
+}
+
+setGifImage(document.getElementById('img-selection-gif'), 'cat');
+document.getElementById('btn-change-photo-gif').addEventListener('click', function() {
+  let query = document.getElementById('txt-photo-gif').value;
+  console.log(query);
+  setGifImage(document.getElementById('img-selection-gif'), query);
+});
+document.getElementById('btn-send-to-friend-gif').addEventListener('click', function() {
+  let img = document.getElementById('img-selection-gif').src;
+  sendMessage(img);
+});
+
 setRandomImage(document.getElementById('img-selection'));
 document.getElementById('btn-change-photo').addEventListener('click', function() {
   setRandomImage(document.getElementById('img-selection'));
-})
+});
+document.getElementById('btn-send-to-friend').addEventListener('click', function() {
+  let img = document.getElementById('img-selection').src;
+  sendMessage(img);
+});
 
 setRandomImage(document.getElementById('img-facebook-photo'), 400, 350);
 setInterval(function(){
@@ -34,3 +91,9 @@ setInterval(function(){
   document.getElementById('number-of-comments').innerText = comments;
   document.getElementById('number-of-shares').innerText = shares;
 }, 3000);
+
+
+
+var myFullpage = new fullpage('#main', {
+  sectionsColor: ['#3C5A99', '#4BBFC3', '#7BAABE', '#ccddff']
+});
