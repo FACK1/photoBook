0956899fb0bb8c2da81cffa367c8d3d14a4ecac0
shaima96 <141062@ppu.edu.ps>
2018-11-07 09:08:20 -0600

    update main section #6

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
-var apiRandomPhoto = 'https://picsum.photos/400/400/?random';
 
-function getRangomImage(fn){
+function getRangomImage(fn, height=400, width=400){
+  var apiRandomPhoto = `https://picsum.photos/${height}/${width}/?random`;
   fetch(apiRandomPhoto)
   .then(function(res){
     fn(res.url)
   });
 }
 
+function setRandomImage(el, height=400, width=400){
+  getRangomImage(function(url){
+      el.src = url;
+  }, height, width);
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-getRangomImage(function(img) {
-  document.getElementById('img-selection').src = img;
-})
+setRandomImage(document.getElementById('img-selection'));
 document.getElementById('btn-change-photo').addEventListener('click', function() {
-  getRangomImage(function(img) {
-    document.getElementById('img-selection').src = img;
-  })
+  setRandomImage(document.getElementById('img-selection'));
 })
+
+setRandomImage(document.getElementById('img-facebook-photo'), 400, 350);
+setInterval(function(){
+  setRandomImage(document.getElementById('img-facebook-photo'), 400, 350);
+  let likes    = document.getElementById('number-of-likes').innerText,
+      shares   = document.getElementById('number-of-shares').innerText,
+      comments = document.getElementById('number-of-comments').innerText;
+  likes = parseInt(likes) + getRandomInt(10);
+  comments = parseInt(comments) + getRandomInt(5);
+  shares = parseInt(shares) + getRandomInt(2);
+  document.getElementById('number-of-likes').innerText = likes;
+  document.getElementById('number-of-comments').innerText = comments;
+  document.getElementById('number-of-shares').innerText = shares;
+}, 3000);
